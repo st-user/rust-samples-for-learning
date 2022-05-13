2c40ea287e6e36d61b441fe4c7175b2140dccf2c
Tomoki Sato <nck.cd.339@gmail.com>
2022-05-13 20:04:48 +0900

    Use mutex on internal data

--- async-event-emitter/src/main.rs
+++ async-event-emitter/src/main.rs
@@ -15,29 +15,41 @@ type HandlerPtr<T> = Box<
         + Sync
 >;
 
+type HandlerRef<T> = Arc<Mutex<HandlerPtr<T>>>;
+
+pub struct EventEmitterInternal<E: Hash + Eq, T> {
+    handlers: Mutex<HashMap<E, Vec<HandlerRef<T>>>>
+}
 
 pub struct EventEmitter<E: Hash + Eq, T> {
-    handlers: HashMap<E, Vec<Arc<Mutex<HandlerPtr<T>>>>>
+    internal: EventEmitterInternal<E, T>
 }
 
 impl<E: Hash + Eq, T: Clone> EventEmitter<E, T> {
     pub fn new() -> Self {
         Self {
-            handlers: HashMap::new()
+            internal: EventEmitterInternal {
+                handlers: Mutex::new(HashMap::new())
+            }
         }
     }
 
-    pub fn on(&mut self, event: E, handler: HandlerPtr<T>)
-    {
-        let event_handlers = self.handlers.entry(event).or_insert_with(|| 
+    pub async fn on(&self, event: E, handler: HandlerPtr<T>)
+    {   
+        let mut handlers = self.internal.handlers.lock().await;
+
+        let event_handlers = handlers.entry(event).or_insert_with(|| 
             vec![]
         );
 
         event_handlers.push(Arc::new(Mutex::new(handler)));
     }
 
-    pub async fn emit(&mut self, event: &E, payload: T) {
-        if let Some(handlers) = self.handlers.get(event) {
+    pub async fn emit(&self, event: &E, payload: T) {
+
+        let handlers = self.internal.handlers.lock().await;
+
+        if let Some(handlers) = handlers.get(event) {
             for handler in handlers.into_iter() {
                 let mut f = handler.lock().await;
                 f(payload.clone()).await;
@@ -49,8 +61,7 @@ impl<E: Hash + Eq, T: Clone> EventEmitter<E, T> {
 #[tokio::main]
 async fn main() {
     
-    let emitter = Arc::new(Mutex::new(EventEmitter::new()));
-
+    let emitter = Arc::new(EventEmitter::new());
 
     let em1 = emitter.clone();
     let h1 = tokio::spawn(async move {
@@ -58,36 +69,36 @@ async fn main() {
         let events = vec!["click".to_owned(), "change".to_owned()];
         for e in events {
             tokio::time::sleep(std::time::Duration::from_secs(1)).await;
-            em1.lock().await.emit(&e, "Hello World!".to_owned()).await;
+            em1.emit(&e, "Hello World!".to_owned()).await;
         }
 
     });
 
     let em2 = emitter.clone();
     let h2 = tokio::spawn(async move {
-        em2.lock().await.on("click".to_owned(), Box::new(move |name: String| {
+        em2.on("click".to_owned(), Box::new(move |name: String| {
 
             println!("Click 1 {}", name);
     
             Box::pin(async {})
-        }));
+        })).await;
 
-        em2.lock().await.on("click".to_owned(), Box::new(move |name: String| {
+        em2.on("click".to_owned(), Box::new(move |name: String| {
 
             println!("Click 2 {}", name);
     
             Box::pin(async {})
-        }));
+        })).await;
     });
 
     let em3 = emitter.clone();
     let h3 = tokio::spawn(async move {
-        em3.lock().await.on("change".to_owned(), Box::new(move |name: String| {
+        em3.on("change".to_owned(), Box::new(move |name: String| {
 
             println!("Change {}", name);
     
             Box::pin(async {})
-        }));
+        })).await;
     });
     
     h1.await.unwrap();
